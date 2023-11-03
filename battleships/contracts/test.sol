921dfca7a93032da59821b750b30d0e6a4192e9e
otherview <otherview@gmail.com>
2023-11-03 18:47:57 +0000

    adding hardhat + contract

---
+++ battleships/contracts/test.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Test {
+
+    uint constant public GRID_SIZE = 20;
+    uint constant public MAX_SHIPS_PER_PLAYER = 1;
+    uint constant public SHIP_SIZE = 3;
+
+}
