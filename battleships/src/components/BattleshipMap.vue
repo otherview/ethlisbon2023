a252d71a695321ca0b4874fd402b5b168c43bbad
otherview <otherview@gmail.com>
2023-11-03 17:06:58 +0000

    bwoop

--- battleships/src/components/BattleshipMap.vue
+++ battleships/src/components/BattleshipMap.vue
@@ -1,34 +1,34 @@
 <template>
   <div>
     <div class="top-banner">
-      <img :src="bannerImg" alt="Top Banner" /> <!-- Update the source path to your image -->
+      <img :src="bannerImg" alt="Top Banner" /> <!-- The banner image is now bound to the bannerImg property -->
     </div>
+    <p>
+      &nbsp;
+    </p>
 
-  <div class="grid-map">
-    <div class="grid" @click="clickCell($event)">
-      <div
-          v-for="(cell, index) in cells"
-          :key="index"
-          class="cell"
-          :data-row="getRow(index)"
-          :data-col="getCol(index)"
-      ></div>
+    <div class="grid-map">
+      <div class="grid" @click="clickCell($event)">
+        <div
+            v-for="(cell, index) in cells"
+            :key="index"
+            class="cell"
+            :data-row="getRow(index)"
+            :data-col="getCol(index)"
+        ></div>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
-
-import bannerImg from "../assets/banner.png";
+// Import the banner image from assets
+import bannerImg from '@/assets/banner.png'; // Adjust the path if necessary
 
 export default {
   name: 'BattleshipMap',
   setup() {
-
-    return {
-      bannerImg,
-    }
+    return { bannerImg };
   },
   data() {
     return {
@@ -49,7 +49,7 @@ export default {
     },
     clickCell(event) {
       const cell = event.target;
-      if(cell.classList.contains('cell')) {
+      if (cell.classList.contains('cell')) {
         const row = cell.dataset.row;
         const col = cell.dataset.col;
         console.log(`Cell clicked: Row ${row}, Column ${col}`);
@@ -60,37 +60,38 @@ export default {
 </script>
 
 <style scoped>
+.top-banner img {
+  width: 100%; /* Full width */
+  object-fit: cover; /* Cover ensures the image covers the area without distortion */
+}
+
 .grid-map {
-  width: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh; /* Use full screen height to center the grid vertically */
-  margin: 0 auto; /* Centers the container */
+  height: calc(100vh - 30vh); /* Subtract the height of the top banner */
+  width: 100%;
+  margin: 0 auto; /* Center the grid map horizontally */
 }
 
 .grid {
   display: grid;
   grid-template-columns: repeat(20, 1fr); /* Creates a 20x20 grid */
   grid-gap: 2px; /* Space between cells */
-  width: 100%; /* Ensure the grid takes the full width of the container */
+  width: 100%; /* Grid takes full width of the container */
 }
 
 .cell {
-  width: 100%; /* 100% of the column width */
-  padding-bottom: 20%; /* Adjust padding to make cells larger */
-  background-color: #eee; /* Cell color */
+  width: 100%; /* Each cell takes up 100% of the column width */
+  padding-bottom: 20%; /* Padding to maintain aspect ratio */
+  background-color: #eee; /* Default cell background color */
   border: 1px solid #ccc; /* Cell border */
   box-sizing: border-box;
-  transition: background-color 0.3s; /* Smooth background color transition */
+  transition: background-color 0.3s; /* Smooth transition for background color */
 }
 
 .cell:hover {
-  background-color: orange; /* Change hover color to orange */
-  cursor: pointer; /* Change cursor to pointer on hover */
-}
-
-.top-banner {
-  max-height: 30vh;
+  background-color: orange; /* Cell background color on hover */
+  cursor: pointer; /* Cursor changes to pointer on hover */
 }
 </style>
